8266dbca256366fb856996c4308723e32f7a3901
naosim <naosms-other@yahoo.co.jp>
2023-11-04 06:46:46 +0900

    stmartphone

--- docs/lib/mylib/footer.mjs
+++ docs/lib/mylib/footer.mjs
@@ -2,9 +2,9 @@
 const seg = location.href.split("index.html")[0].split("/").filter(v => v.trim().length > 0).at(-1);
 
 const text = `
-<p>© 2023 なおしむ</p>
 <p><a href="https://github.com/naosim/p5sample/tree/main/docs/${seg}" target="_blank">ソースコード</a>
 <p><a href="https://github.com/naosim/p5sample" target="_blank">トップページ</a>
+<p>© 2023 なおしむ</p>
 `
 const footer = document.createElement("footer");
 footer.innerHTML = text;
